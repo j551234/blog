8133b64bf787fd47a60c9270fe39bec1cc26cde9
MNL <10030812peter@gmail.com>
2017-04-19 23:03:40 +0800

    contact us更改 還要待修

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,49 +12,54 @@
                     <hr class="primary">
                 </div>
                 
-                <div id="message">
-					
-												
-					<form method="post" action="">
-					<ul class="dataCols">			
-		
-						<li>
-							<h4><span class="required">*</span><label>問題類別</label></h4>
-							<SELECT NAME="cat_code" required="required">
-							<OPTION VALUE="">請選擇</OPTION>
-														<OPTION VALUE="1" >產品詢問</OPTION>
-														<OPTION VALUE="2" >訂貨詢問</OPTION>
-														<OPTION VALUE="3" >退貨詢問</OPTION>
-														<OPTION VALUE="4" >其它</OPTION>
-														</SELECT>	
-						</li>
-						<li>
-							<h4><span class="required">*</span><label>姓名</label></h4>
-							<input type="text" size="10" name="name" value="" class="" required="required">
-						</li>
-						<li>
-							<h4><span class="required">*</span><label>聯絡電話</label></h4>
-							<input type="text" size="10" name="tel" value="" required="required" class="">
-						</li>
-						<li>
-							<h4><span class="required">*</span><label>Email</label></h4>
-							<input type="text" size="30" name="email" value="" required="required" class="width100">						
-						</li>						
+            <article>
 
-						<li>
-							<h4><span class="required">*</span><label>內容</label></h4>
-							<textarea cols="95" rows="5" name="question"  required="required"></textarea>
-						</li>
-						<li>
-							<h4></h4>
-							<input type="submit" name="send" class="btn bnt-submit" style="height:35px;" value="送出">
-						</li>
-					</ul>
-					</form>
-					
-				</div>
+			<div class="content">
 
-			<div>
+				<p>若您有什麼問題，請填寫下列表格並送出，我們會盡快聯絡您，謝謝! </p>
+
+				<form id="contact-form" name="contact-form" enctype="multipart/form-data" method="post" action="https://stuntbox.wufoo.com/forms/r7x3q1/#public">
+
+					<fieldset>
+
+						<label class="contact-type" for="Field5">What kind of problem?</label>
+						<select class="contact-type" id="Field5" name="Field5" >
+							<option value="Function Problem" selected>功能問題</option>
+							<option value="Product Problem">產品問題</option>
+							<option value="Other">其他</option>
+						</select>
+
+						
+
+						<label class="contact-name" for="Field2">Name</label>
+						<input class="contact-name" id="Field2" name="Field2" type="text" maxlength="255" />
+
+						<label class="contact-email" for="Field7">Email Address</label>
+						<input class="contact-email" id="Field7" name="Field7" type="email" spellcheck="false" maxlength="255" tabindex="4" required data-error-msg="Please enter a valid email address." />
+
+						<label class="contact-phone" for="Field4">Phone Number</label>
+						<input class="contact-phone" id="Field4" name="Field4" type="text" maxlength="255"/>
+						
+						<label class="contact-message" for="Field1">Message</label>
+						<textarea class="contact-message" id="Field1" name="Field1" spellcheck="true" required data-error-msg="Please enter a message."></textarea>
+
+					    <input class="submit-btn" name="saveForm" type="submit" value="Send Message" />
+
+						<div class="hide">
+							<label for="comment">Do Not Fill This Out</label>
+							<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+							<input type="hidden" id="idstamp" name="idstamp" value="yFYl0eV7r0QSSz4/gkRlqrQqKQq490aGeCqHFl7P6HY=" />
+						</div>
+
+					</fieldset>
+
+				</form>
+
+			</div><!-- end .content -->
+
+		    </article>
+
+	    <div>
                 <div class="col-lg-4 text-center">
                     <i class="fa fa-phone fa-3x sr-contact"></i>
                     <p>123-456-6789</p>
