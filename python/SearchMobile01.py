029fca831b9f5d879b9d0294ffb251bfadf053a2
wlunareve <wlunareve@gmail.com>
2017-05-07 18:08:20 +0800

    SearchMobile01 update

--- python/SearchMobile01.py
+++ python/SearchMobile01.py
@@ -42,44 +42,39 @@ for change in range(2,5):
         res=requests.get(search_href)
         res.encoding='utf-8'
         soup2 = BeautifulSoup (res.text, "lxml")
-        for Main_article in soup2.find_all('main'):
+        href_split=search_href.split('%')
+        if href_split[0]=='https://www.google.com//url?q=https://www.mobile01.com/topicdetail.php'or href_split[0]=='https://www.google.com//url?q=https://www.mobile01.com/waypointtopicdetail.php':
+            Main_article = soup2.find_all('main')
+            #標題
+            search_title=Main_article[0].select('.topic')[0].text
+            print(search_title)
+            #作者、作者網址
+            authors = Main_article[0].select('.inner')
+            author_name = authors[0].select('.fn')
+            search_author=author_name[0].find('a').text
+            author_href=mobile01_url+author_name[0].find('a')['href']
 
-            #ChromeDriver
-            #href_split=search_href.split('%')
-            #if href_split[0]=='https://www.mobile01.com/topicdetail.php':
-            #
-            #PhantomJS
-            href_split=search_href.split('%')
-            if href_split[0]=='https://www.google.com//url?q=https://www.mobile01.com/topicdetail.php':
-                #標題
-                search_title=Main_article.select('.topic')[0].text
-                #作者、作者網址
-                authors = Main_article.select('.inner')
-                author_name = authors[0].select('.fn')
-                search_author=author_name[0].find('a').text
-                author_href=mobile01_url+author_name[0].find('a')['href']
-                
-                #人氣
-                info=authors[0].select('.info')
-                search_view=info[0].text.strip('文章人氣:').lstrip().replace(',','')
-                #文章時間
-                time1=authors[0].select('.date')
-                search_time=time1[0].text.strip('#1')
-                #文章圖片
-                post = Main_article.select('.single-post-content')
-                if post[0].find('img')!=None:
-                    picture=post[0].find('img')['src'].strip()
-                    split=picture.split('/')
-                    if split[2]=='attach.mobile01.com':
-                        article_picture='https:'+picture
-                    elif split[2]=='download.mobile01.com':
-                        article_picture='https:'+picture
-                    elif split[2]=='attach2.mobile01.com':
-                        article_picture='./img/mobile01.jpg'
-                    else:
-                        article_picture=picture
-                else:
+            #人氣
+            info=authors[0].select('.info')
+            search_view=info[0].text.strip('文章人氣:').lstrip().replace(',','')
+            #文章時間
+            time1=authors[0].select('.date')
+            search_time=time1[0].text.strip('#1')
+            #文章圖片
+            post = Main_article[0].select('.single-post-content')
+            if post[0].find('img')!=None:
+                picture=post[0].find('img')['src'].strip()
+                split=picture.split('/')
+                if split[2]=='attach.mobile01.com':
+                    article_picture='https:'+picture
+                elif split[2]=='download.mobile01.com':
+                    article_picture='https:'+picture
+                elif split[2]=='attach2.mobile01.com':
                     article_picture='./img/mobile01.jpg'
+                else:
+                    article_picture=picture
+            else:
+                article_picture='./img/mobile01.jpg'
                 cur.execute(sqli,(chinese_key_word,search_title,search_href,search_time,search_author,article_picture,author_href,search_view)) #存入資料庫    
                 conn.commit()
         time.sleep(0.5)
