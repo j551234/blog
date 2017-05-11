601b9d5028263e9a683a5b2fac0b180d9d641044
yao-ching <wang7186w@yahoo.com.tw>
2017-05-11 18:27:27 +0800

    顏色一波

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -125,19 +125,19 @@
                         let Changecolor=document.querySelectorAll(".text-primary")
                         switch(tag){
                             case "food":{
-                                Changecolor[0].style.color="#6a91b9"
+                                Changecolor[0].style.color="#F05F40"
                                 break
                             }
                             case "dress":{
-                                Changecolor[1].style.color="#6a91b9"
+                                Changecolor[1].style.color="#F05F40"
                                 break
                             }
                             case "travel":{
-                                Changecolor[2].style.color="#6a91b9"
+                                Changecolor[2].style.color="#F05F40"
                                 break
                             }
                             case "digital":{
-                                Changecolor[3].style.color="#6a91b9"
+                                Changecolor[3].style.color="#F05F40"
                                 break
                             }
                         }
