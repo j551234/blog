d6896f3ed24363d0e90f2f1cfd7f5c10e714f6da
yao-ching <wang7186w@yahoo.com.tw>
2017-05-12 14:15:36 +0800

    all_index & delete * index

---
+++ python/To_all_Index.bat
@@ -0,0 +1,6 @@
+cd C:/xampp/htdocs/project/python
+python C:/xampp/htdocs/project/python/IndexXuite.py
+python C:/xampp/htdocs/project/python/IndexMobile01.py
+python C:/xampp/htdocs/project/python/IndexPixnet.py
+python C:/xampp/htdocs/project/python/IndexPtt.py
+exit
